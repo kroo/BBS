9a1ac038e5cef536a1109464a302cae83c4436c8
Elliot Kroo <elliot.kroo@gmail.com>
2010-04-12 23:52:47 -0700

    committing results of jboost run

--- asphaltClassifier/asphaltClassifier.m
+++ asphaltClassifier/asphaltClassifier.m
@@ -1,43 +1,45 @@
 
 %%
 % Generate scatter plots of pixel values
-ParkingLot = imread('data/parking_lot/image.png');
-Mask = imread('data/parking_lot/mask.png');
+ParkingLot = imread('../data/parking_lot/image.png');
+Mask = imread('../data/parking_lot/mask.png');
 
-h_ParkingLot = imshow(ParkingLot); 
+%imshow(ParkingLot);
 
 %%
 BW = im2bw(Mask);
 inverseBW = 1-BW;
 
-figure;
+% figure;
 
-imshow(inverseBW);
+%imshow(inverseBW);
 %%
 PicHSV=rgb2hsv(ParkingLot);
 PicH=PicHSV(:,:,1);
 PicS=PicHSV(:,:,2);
 PicV=PicHSV(:,:,3);
 
-imshow(ParkingLot); 
-figure;
-imshow(PicH);
-figure;
-imshow(PicS);
-figure;
-imshow(PicV);
+%imshow(ParkingLot); 
+%figure;
+%imshow(PicH);
+%figure;
+%imshow(PicS);
+%figure;
+%imshow(PicV);
 
 %%
 
 fid = fopen('asphalt.data','w');
 fraction=.03;
-negativetrainingset= if (rand < fraction && its false ) %put that thing into a matrix
-    
+%negativetrainingset= if (rand < fraction && its false ) %put that thing into a matrix
+nNegativeTrainingExamples = size(inverseBW(inverseBW==0),1);
+nPositiveTrainingExamples = size(inverseBW(inverseBW==1),1);
+
 [m,n,k] = size(PicHSV);
 for i=1:m
     for j=1:n;
-        if(inverseBW(i,j)==0)
-            if(rand>0.03)
+        if(inverseBW(i,j)==1)
+            if(rand>nNegativeTrainingExamples/nPositiveTrainingExamples)
                 continue;
             end
         end
@@ -45,8 +47,7 @@ for i=1:m
     end
 end
 fclose(fid);
-
-
+disp('done');
 
 
 
--- runjboost.sh
+++ runjboost.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 export JBOOST_DIR="`pwd`/jboost-2.1" 
 export CLASSPATH="$CLASSPATH:$JBOOST_DIR/dist/jboost.jar:$JBOOST_DIR/lib/jfreechart-1.0.10.jar:$JBOOST_DIR/lib/jcommon-1.0.8.jar"
+
+java -Xmx500M jboost.controller.Controller -S asphaltClassifier/asphalt
