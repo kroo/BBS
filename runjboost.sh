48c5dacf374f9b9b5bc96e952a3e6aac11fafe0a
Elliot Kroo <elliot.kroo@gmail.com>
2010-04-13 01:55:44 -0700

    Long investigation of the asphalt classifier

--- runjboost.sh
+++ runjboost.sh
@@ -2,4 +2,4 @@
 export JBOOST_DIR="`pwd`/jboost-2.1" 
 export CLASSPATH="$CLASSPATH:$JBOOST_DIR/dist/jboost.jar:$JBOOST_DIR/lib/jfreechart-1.0.10.jar:$JBOOST_DIR/lib/jcommon-1.0.8.jar"
 
-java -Xmx500M jboost.controller.Controller -S asphaltClassifier/asphalt
+java -Xmx500M jboost.controller.Controller -S asphaltClassifier/asphalt -m asphaltClassifier.m
