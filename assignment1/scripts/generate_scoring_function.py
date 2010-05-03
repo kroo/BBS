57f23bac8e587042373020707e95dc3e82f2a9ff
Jeanne <jeanne@hoyden.dynamic.ucsd.edu>
2010-05-03 12:44:50 -0700

    scripts for assignment1

--- assignment1/scripts/generate_scoring_function.py
+++ assignment1/scripts/generate_scoring_function.py
@@ -1,58 +1,35 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 
 import sys, os
 
-# check for arguments
+# check for inputs
 if len(sys.argv) == 1:
     print 'Usage: Pass in an image and an xml file'
     print 'Run from same directory as passed files'
     sys.exit()
 
-# directory management
-scripts = sys.path[0]   # full path to scripts
-dir = os.getcwd()       # full path to working directory
-title = os.path.split(dir)[1]   # short name of working directory
-temp = dir + '/' + title + '_temp'    # full path to temp directory
-os.mkdir(temp)
-
-# prepare MATLAB input
+# store inputs
 image = sys.argv[1]
 xml = sys.argv[2]
-data = temp + '/' + title + '.data'
 
-# run MATLAB script to prepare data
-### do I need to add a path for MATLAB to get to /scripts? ###
-matlab_command = "prepare_data('" + image + "','" + xml + "','" + data + "')"
-os.system('matlab -r "' + matlab_command + '" -nosplash -wait')
+# store paths, make temp directory
+script_path = os.path.join(os.path.dirname(sys.path[0]),'jboost-2.1','scripts')
+###script_path = 'C:\Users\Alex\BBS\jboost-2.1\scripts'
+###script_path = '/Users/jeanne/school/cse151/BBS/jboost-2.1/scripts/' 
+working_path = os.getcwd()
+temp_path = os.path.join(working_path,'temp2')
+os.mkdir(temp_path)
 
-# permute data
-os.system('python ' + scripts + '/permute_data.py --filename=' + data)
+# stem is a movie name (10 digit number)
+stem = os.path.basename(working_path)
 
-# separate data
-os.chdir(temp)
-lines = open(title + '.data.P').readlines()
-file_test = open(title + '.test','w')
-file_train = open(title + '.train','w')
-for i,l in enumerate(lines):
-    if i < len(lines)/2:
-        file_test.write(l)
-    else:
-        file_train.write(l)
-file_test.close()
-file_train.close()
+# call MATLAB to get data
+data = os.path.join(temp_path,stem + '.data')
+matlab_command = "prepare_data('%s','%s','%s')"%(image,xml,data)
+matlab_options = '-nodisplay -nodesktop -nosplash -wait'
+os.system('matlab -maci %s -r "%s" &'%(matlab_options,matlab_command))
 
-# generate spec file
-file_spec = open(title + '.spec','w')
-file_spec.write("exampleTerminator=;\nattributeTerminator=,\nmaxBadExa=0\n")
-file_spec.write("labels (0,1)\nH  number\nS  number\nV  number")
-file_spec.close()
-os.chdir(dir)
 
-# run jboost to generate scoring function
-os.chdir('../../jboost-2.1')
-jboost_command = 'java jboost.controller.Controller '
-jboost_input = '-S ' + temp + '/' + title + ' '
-os.system(jboost_command + jboost_input + '-m ' + dir + '/scoring_function.m' )
 
 
 
--- assignment1/scripts/pytest.py
+++ assignment1/scripts/pytest.py
@@ -6,5 +6,6 @@ print 'sys.path[0] = ' + sys.path[0]
 print 'os.path.join(sys.path[0], sys.argv[0]) = ' + os.path.join(sys.path[0], sys.argv[0])
 
 print 'os.getcwd() = ' + os.getcwd()
-#for i,j in enumerate(sys.path):
-#    print 'sys.path[' + str(i) + '] = ' + j
+
+a = os.path.join(os.path.dirname(sys.path[0]),'jboost-2.1')
+print 'complicated: ', a
