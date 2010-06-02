a740986e59afa0ff06ecde0c391531649eb3efc9
Elliot Kroo <elliot.kroo@gmail.com>
2010-06-01 23:03:10 -0700

    now runs at about 5fps, and builds up a mosaic of the ground from a given perspective

--- imageRegistration/bbs_registration.cpp
+++ imageRegistration/bbs_registration.cpp
@@ -152,14 +152,20 @@ void calcNecessaryImageRotation(IplImage *src) {
 
 }
 
-const int MAX_CORNERS = 500;
-void processImagePair(const char *file1, const char *file2, CvVideoWriter *out, struct CvMat *currentOrientation) {
+const int MAX_CORNERS = 10;
+static IplImage* trans_image = 0; // draw everything onto a shared canvas
+
+// void processImagePair(const char *file1, const char *file2, CvVideoWriter *out, struct CvMat *currentOrientation) {
+void processImagePair(int num, IplImage *imgAcolor, IplImage *imgBcolor, CvVideoWriter *out, struct CvMat *currentOrientation) {
+	CvSize img_sz = cvGetSize( imgAcolor );
+
   // Load two images and allocate other structures
-	IplImage* imgA = cvLoadImage(file1, CV_LOAD_IMAGE_GRAYSCALE);
-	IplImage* imgB = cvLoadImage(file2, CV_LOAD_IMAGE_GRAYSCALE);
-	IplImage* imgBcolor = cvLoadImage(file2);
+	IplImage* imgA = cvCreateImage(img_sz, 8, 1);//cvLoadImage(file1, CV_LOAD_IMAGE_GRAYSCALE);
+	IplImage* imgB = cvCreateImage(img_sz, 8, 1);
+
+  cvCvtColor(imgAcolor, imgA, CV_RGB2GRAY);
+  cvCvtColor(imgBcolor, imgB, CV_RGB2GRAY);
  
-	CvSize img_sz = cvGetSize( imgA );
 	int win_size = 15;
   
 	// Get the features for tracking
@@ -172,7 +178,7 @@ void processImagePair(const char *file1, const char *file2, CvVideoWriter *out, 
 	cvGoodFeaturesToTrack( imgA, eig_image, tmp_image, cornersA, &corner_count,
 		0.05, 3.0, 0, 3, 0, 0.04 );
  
-  fprintf(stderr, "%s: Corner count = %d\n", file1, corner_count);
+  fprintf(stderr, "frame %d: Corner count = %d\n", num, corner_count);
  
 	cvFindCornerSubPix( imgA, cornersA, corner_count, cvSize( win_size, win_size ),
 		cvSize( -1, -1 ), cvTermCriteria( CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 50, 0.03 ) );
@@ -188,7 +194,7 @@ void processImagePair(const char *file1, const char *file2, CvVideoWriter *out, 
  
 	CvPoint2D32f* cornersB = new CvPoint2D32f[ MAX_CORNERS ];
  
-  calcNecessaryImageRotation(imgA);
+  // calcNecessaryImageRotation(imgA);
  
 	cvCalcOpticalFlowPyrLK( imgA, imgB, pyrA, pyrB, cornersA, cornersB, corner_count, 
 		cvSize( win_size, win_size ), 5, features_found, feature_errors,
@@ -203,22 +209,34 @@ void processImagePair(const char *file1, const char *file2, CvVideoWriter *out, 
 
    cvInvert(transform, invTransform);
    cvMatMul(currentOrientation, invTransform, currentOrientation);
-   // save the translated image
- 	 IplImage* trans_image = cvCloneImage(imgBcolor);
-   cvWarpPerspective(imgBcolor, trans_image, currentOrientation, CV_INTER_CUBIC+CV_WARP_FILL_OUTLIERS);
 
-   printf("%s:\n", file1);
-   PrintMat(currentOrientation);
+   CvMat *tf_scaled_offset = cvCloneMat(currentOrientation);
+   CvMat *scaled_down = cvCreateMat(3,3, CV_32FC1);
+   cvSetIdentity(scaled_down);
+   // cvSet2D(scaled_down, 0,0, cvScalar(0.5)); // 0.5 0.0 300
+   // cvSet2D(scaled_down, 1,1, cvScalar(0.5)); // 0.0 0.5 300
+   cvSet2D(scaled_down, 2,2, cvScalar(2.0)); // 0.0 0.5 300
+   cvSet2D(scaled_down, 0,2, cvScalar(200));
+   cvSet2D(scaled_down, 1,2, cvScalar(200));
+   PrintMat(scaled_down);
+   cvMatMul(scaled_down, currentOrientation, tf_scaled_offset);
+
+   // save the translated image
+   if (!trans_image) trans_image = cvCreateImage( img_sz, 8, 3 );
+   cvWarpPerspective(imgBcolor, trans_image, tf_scaled_offset, CV_INTER_CUBIC);
+
+   printf("%d:\n", num);
+   // PrintMat(currentOrientation);
 
   // cvSaveImage(out, trans_image);
   cvWriteFrame(out, trans_image);
 
   cvReleaseImage(&eig_image);
   cvReleaseImage(&tmp_image);  
-  cvReleaseImage(&trans_image);
+  // cvReleaseImage(&trans_image);
   cvReleaseImage(&imgA);
   cvReleaseImage(&imgB);
-  cvReleaseImage(&imgBcolor);
+  // cvReleaseImage(&imgBcolor);
   cvReleaseImage(&pyrA);
   cvReleaseImage(&pyrB);
   
@@ -277,24 +295,46 @@ int main(int argc, char* argv[])
 
 static IplImage* lastImage = 0;
 static bool firstTime = true;
-void ProcessImage(AVFrame *pFrame, int width, int height, int frameno) {
+// to change this, run this script (in python)!
+// print "static unsigned char gamma_mapping[] = {", ", ".join(["%d" % int(float(i/256.) ** (2.2) * 256) for i in range(0,256)]), "};"
+static unsigned char gamma_mapping[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 12, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 20, 20, 21, 22, 22, 23, 23, 24, 25, 25, 26, 26, 27, 28, 28, 29, 30, 30, 31, 32, 33, 33, 34, 35, 36, 36, 37, 38, 39, 39, 40, 41, 42, 43, 44, 44, 45, 46, 47, 48, 49, 50, 51, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 70, 71, 72, 73, 74, 75, 76, 77, 78, 80, 81, 82, 83, 84, 86, 87, 88, 89, 91, 92, 93, 94, 96, 97, 98, 100, 101, 102, 104, 105, 106, 108, 109, 110, 112, 113, 115, 116, 117, 119, 120, 122, 123, 125, 126, 128, 129, 131, 132, 134, 135, 137, 139, 140, 142, 143, 145, 147, 148, 150, 152, 153, 155, 157, 158, 160, 162, 163, 165, 167, 169, 170, 172, 174, 176, 177, 179, 181, 183, 185, 187, 188, 190, 192, 194, 196, 198, 200, 202, 204, 206, 208, 210, 212, 214, 216, 218, 220, 222, 224, 226, 228, 230, 232, 234, 236, 238, 240, 242, 245, 247, 249, 251, 253 };
+static CvMat *gammaMappingMat = 0;
+void SetUpProcessing() {
+  gammaMappingMat = cvCreateMatHeader( 1, 256, CV_8UC1 );
+  cvSetData( gammaMappingMat, gamma_mapping, 0 );
+}
+
+void ProcessImage(AVFrame *pFrame, int width, int height, int frameno, CvMat *orientation, CvVideoWriter *writer) {
+  if(!gammaMappingMat) SetUpProcessing();
+  
+  // set up an IplImage from the AVFrame
   IplImage* myIplImage = cvCreateImageHeader(cvSize(width,height), IPL_DEPTH_8U, 3);
   void *ptr = (void *)(pFrame->data[0]);
   cvSetData(myIplImage,ptr,width*3);
   
+  // duplicate the image, to apply a gamma mapping
+  IplImage* adjustedImage = cvCloneImage(myIplImage);
+  
+  // cvLUT(myIplImage, adjustedImage, gammaMappingMat);
+    
   // process image:
-  if(lastImage && firstTime) {
+  if(lastImage) {
     // we have an image pair to process
     printf("lastImage: %p\n", lastImage);
-    firstTime = false;
-  
-    cvSaveImage("output_image.png", myIplImage);
+    // firstTime = false;
+    
+    processImagePair(frameno, lastImage, adjustedImage, writer, orientation);
+    
+    // cvSaveImage("output_image.png", adjustedImage);
+    printf("processed: %d\n", frameno);
+    
   }
 
   // clean up
+  cvReleaseImageHeader(&myIplImage);
   if(lastImage)
-    cvReleaseImageHeader(&lastImage);
-  lastImage = myIplImage;
+    cvReleaseImage(&lastImage);
+  lastImage = adjustedImage;
 }
 
 
@@ -308,11 +348,26 @@ int ReadVideo(const char *filename) {
     int             numBytes;
     uint8_t         *buffer;
 
-    int64_t firstFrame = 15000;
-    int64_t lastFrame = 16000;
+    int64_t firstFrame = 22560;
+    int64_t lastFrame = 26640;
 
+    printf("Initting video output...\n");
+    int frame = 11;
+    CvMat *orientation = cvCreateMat(3,3,CV_32FC1);
+    cvSetIdentity(orientation);
+    CvVideoWriter *writer = 0;
+    int isColor = 1;
+    int fps     = 30;
+    int frameW  = 1280;
+    int frameH  = 720;
+    
     // Register all formats and codecs
     av_register_all();
+    
+    
+    writer=cvCreateVideoWriter("/Users/elliot/Desktop/out.avi",CV_FOURCC('P','I','M','1'),
+      fps,cvSize(frameW,frameH),isColor);
+    printf("finished: %p\n", writer);
 
     // Open video file
     if(av_open_input_file(&pFormatCtx, filename, NULL, 0, NULL)!=0)
@@ -388,6 +443,7 @@ int ReadVideo(const char *filename) {
     
     // Read frames and save first five frames to disk
     i=0;
+        
     while(GetNextFrame(pFormatCtx, pCodecCtx, videoStream, pFrame))
     {
       sws_scale(swsContext,
@@ -397,9 +453,11 @@ int ReadVideo(const char *filename) {
         if(i%100 == 0) printf("frame %lld reached\n", i+firstFrame);
         // Save the frame to disk
         if(++i<=lastFrame-firstFrame && i>0)
-          ProcessImage(pFrameBGR, pCodecCtx->width, pCodecCtx->height, i);
+          ProcessImage(pFrameBGR, pCodecCtx->width, pCodecCtx->height, i, orientation, writer);
         else if(i>(lastFrame-firstFrame)) break;
     }
+    
+    cvReleaseVideoWriter(&writer);
 
     // Free the BGR image
     delete [] buffer;
